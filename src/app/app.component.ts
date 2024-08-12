2d6f0d9b808d841a3edc534f974357b7b2be19ef
Yaren Durmaz <yarendrmz594@gmail.com>
2024-08-12 15:28:17 +0300

    v4
    Sayfanın sağında bulunan favoriler ve koyu mod kutusunun düzenlenmesi yapıldı. Kelimelerin okunuşu ve seslerini API'den doğru çekmiyordu. Onun düzeltilmesi yapıldı. Genel olarak koyu mod üzerinde düzenleme yapıldı.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { DictionaryService } from './dictionary.service';
 
 interface Phonetic {
@@ -20,7 +21,6 @@ interface Meaning {
 
 interface DictionaryEntry {
   word: string;
-  phonetic: string;
   phonetics: Phonetic[];
   origin: string;
   meanings: Meaning[];
@@ -36,17 +36,31 @@ export class AppComponent {
   tempWord: string = '';
   definition: DictionaryEntry | null = null;
   word: string = '';
-  constructor(private dictionaryService: DictionaryService) {}
+  isDarkMode: boolean = false;
+
+  constructor(
+    private dictionaryService: DictionaryService,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {}
 
   searchDefinition() {
     this.word = this.tempWord;
     if (this.word) {
       this.dictionaryService.getDefinition(this.word).subscribe(
         (data) => {
+          this.definition = null;
+
           if (data && data.length > 0) {
-            this.definition = data[0];
-          } else {
-            this.definition = null;
+            for (let i = 0; i < data.length; i++) {
+              if (data[i]) {
+                this.definition = data[i];
+                break;
+              }
+            }
+          }
+
+          if (!this.definition) {
+            console.warn('No valid definition found in the data.');
           }
         },
         (error) => {
@@ -60,5 +74,24 @@ export class AppComponent {
   playAudio(audioUrl: string) {
     let audio = new Audio(audioUrl);
     audio.play();
+  }
+
+  toggleTheme() {
+    if (isPlatformBrowser(this.platformId)) {
+      this.isDarkMode = !this.isDarkMode;
+      if (this.isDarkMode) {
+        document.body.classList.add('dark-mode');
+        document.body.classList.remove('light-mode');
+      } else {
+        document.body.classList.add('light-mode');
+        document.body.classList.remove('dark-mode');
+      }
+    }
+  }
+
+  ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      document.body.classList.add('light-mode');
+    }
   }
 }
